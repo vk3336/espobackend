ec51bb0f9730db0f1cba73652027006ccd630c90
urvikalal <urvi.qodic@gmail.com>
2025-12-31 11:54:13 +0530

    done

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const productsRouter = require("./routes/products");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
